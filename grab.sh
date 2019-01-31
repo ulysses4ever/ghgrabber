428e332f71b85c3e7206f67b5435b5ae61273c80
Konrad Siek <konrad.siek@gmail.com>
2019-01-31 13:22:56 +0100

    Slightly better name handling.

--- grab.sh
+++ grab.sh
@@ -7,18 +7,33 @@ function download_repo_contents {
 }
 
 function retrieve_commit_metadata {
-    echo '"hash","author name","author email","author timestamp","committer name","committer email","committer timestamp"'
-    git log --pretty=format:'"%H","%an","%ae","%ad","%cn","%ce","%ct"' --all
+    #echo '"hash","author name","author email","author timestamp","committer name","committer email","committer timestamp"'
+    git log --pretty=format:'"%H","%aN","%ae",%at,"%cN","%ce",%ct' --all
 }
 
 function retrieve_commit_file_modification_info {
-    echo '"hash","added lines", "deleted lines", "file"'
+    #echo '"hash","added lines", "deleted lines", "file"'
     git log --pretty=format:-----%H:::  --all --numstat | awk -f "${home}/numstat.awk"
 }
 
 function retrieve_commit_comments {
-    echo '"hash","topic","message"'
+    #echo '"hash","topic","message"'
     git log --pretty=format:"-----%H:::%s:::%B"  --all | awk -f "${home}/comment.awk"
+}
+
+function retrieve_commit_parents {
+    #echo '"child","parent"'
+    git log --pretty=format:"%H %P" | awk '{for(i=2; i<=NF; i++) print "\""$1"\",\""$i"\""}'
+}
+
+function retrieve_commit_repositories {
+    #echo '"hash","id"'
+    git log --pretty=format:"\"%H\",$1"
+}
+
+function retrieve_repository_info {
+    #echo '"id", "user", "project"'
+    echo "${3},\"${1}\",\"${2}\""
 }
 
 function prepare_directories {
@@ -26,12 +41,15 @@ function prepare_directories {
     mkdir -p commit_metadata
     mkdir -p commit_files
     mkdir -p commit_comments
+    mkdir -p commit_parents
+    mkdir -p commit_repositories
+    mkdir -p repository_info
 }
-
 
 function process_repository {
     local user="$1"
     local repo="$2"
+    local i="$3"
 
     local filename="${user}_${repo}.csv"
 
@@ -41,8 +59,16 @@ function process_repository {
     retrieve_commit_metadata > "${home}/commit_metadata/${filename}"
     retrieve_commit_file_modification_info > "${home}/commit_files/${filename}"
     retrieve_commit_comments > "${home}/commit_comments/${filename}"
+    retrieve_commit_parents > "${home}/commit_parents/${filename}"
+    retrieve_commit_repositories $i > "${home}/commit_repositories/${filename}"
+    retrieve_repository_info $user $repo $i > "${home}/repository_info/${filename}"
 
     cd "$home"
+
+    if expr ${repository_path} : '/tmp/tmp\...........' >/dev/null
+    then
+        rm -rfv "${repository_path}"
+    fi
 }
 
 function print_time {
@@ -58,18 +84,25 @@ function print_time {
     echo ${hours}:${min_extra_zero}${minutes}:${sec_extra_zero}${seconds}
 }
 
+echo 'Hi!'
+
 prepare_directories
 echo '"user","repo","time"' > timing.csv
+
+i=0
 for info in `cat repos.list`
 do
-    echo processing $info
+    i=$(( $i + 1 ))
+
+    echo processing $i $info
 
     user=$(echo $info | cut -f1 -d/)
     repo=$(echo $info | cut -f2 -d/)
     
     start_time=$(date +%s)
-    process_repository $user $repo
+    process_repository $user $repo $i
     end_time=$(date +%s)
-    echo "\"${user}\"","\"${repo}\"",$(print_time $((end_time - start_time))) >> timing.csv
+    echo "\"${user}\"","\"${repo}\"",$(print_time $((end_time - start_time)) ) >> timing.csv
 done
- 
+
+
