435b97fa4de604b1291eb5db8b92aa8c7ea7896e
Konrad Siek <konrad.siek@gmail.com>
2019-03-04 15:31:16 +0100

    Parallelization.

--- grab.sh
+++ grab.sh
@@ -1,67 +1,54 @@
 #!/bin/bash
 
+# Timing helper functions
+function timing_init   { 
+    echo "\"$1\",\"$2\",\"time\"" > "$3"; 
+}
+function timing_start  { date +%s; }
+function timing_end    { date +%s; }
+function timing_print  {
+    local time="$1"
+
+    local hours=$((time/60/60))
+    local minutes=$(((time/60)%60))
+    local seconds=$((time%60))
+
+    local sec_extra_zero=`((seconds > 9)) && echo -n "" || echo -n 0`
+    local min_extra_zero=`((minutes > 9)) && echo -n "" || echo -n 0`
+
+    echo ${hours}:${min_extra_zero}${minutes}:${sec_extra_zero}${seconds}
+}
+function timing_output { 
+    echo "\"${1}\"","\"${2}\"",$(timing_print $((${4} - ${3}))) \
+        >> "$5"
+}
+
+# Misc. auxiliary functions.
 function escape_quotes {
     echo "$1" | sed 's/\\/\\\\/g; s/"/\\"/g'
 }
-
 function err_echo {
     echo $@ >&2
 }
-
-function download_repo_contents {
-    err_echo [[ downloading repo contents ]]
-    local destination=$(mktemp --directory)
-    GIT_TERMINAL_PROMPT=0 git clone "https://github.com/$1/$2.git" "$destination"
-    #git clone "ssh://git@github.com/$1/$2.git" "$destination"
-    echo "$destination"
+function use_first_if_available {
+    [ -n "$1" ] && echo "$1" || echo "$2"
 }
 
-function retrieve_commit_metadata {
-    err_echo [[ retrieving commit metadata ]]
-    echo '"hash","author name","author email","author timestamp","committer name","committer email","committer timestamp","tag"'
-    git log --pretty=format:'"%H","%an","%ae","%at","%cn","%ce","%ct","%s","%D"' --all 
-}
+# Toplevel functions that set up the scraper.
+function prepare_globals {
+    export REPOS_LIST=`use_first_if_available "$1" "repos.list"`
+    export OUTPUT_DIR=`use_first_if_available "$2" "data"`
+    export GHGRABBER_HOME="$(pwd)"
 
-function retrieve_commit_file_modification_info {
-    err_echo [[ retrieving commit file modification info ]]
-    echo '"hash","added lines","deleted lines","file"'
-    git log --pretty=format:-----%H:::  --numstat --all | \
-        awk -f "${home}/awk/numstat.awk"
-}
+    processed=0
 
-function retrieve_commit_file_modification_hashes {
-    err_echo [[ retrieving commit file modification hashes ]]
-    echo '"hash","source file hash","current file hash","status code","file"'
-    git log --format="%n%n%h" --raw --abbrev=40 --all | \
-        awk -f "${home}/awk/raw.awk"
+    if expr "$OUTPUT_DIR" : "^/" >/dev/null 
+    then 
+        :
+    else 
+        OUTPUT_DIR="$GHGRABBER_HOME/$OUTPUT_DIR"
+    fi
 }
-
-function retrieve_commit_comments {
-    err_echo [[ retrieving commit messages ]]
-    echo '"hash","topic","message"'
-    git log --pretty=format:"-----%H:::%s:::%B"  --all | \
-        awk -f "${home}/awk/comment.awk"
-}
-
-function retrieve_commit_parents {
-    err_echo [[ retrieving commit parents ]]
-    echo '"child","parent"'
-    git log --pretty=format:"%H %P" | \
-        awk '{for(i=2; i<=NF; i++) print "\""$1"\",\""$i"\""}'
-}
-
-function retrieve_commit_repositories {
-    err_echo [[ retrieving commit repositories ]]
-    echo '"hash","id"'
-    git log --pretty=format:"\"%H\",$1"
-}
-
-function retrieve_repository_info {
-    err_echo [[ retrieving repository info ]]
-    echo '"id","user","project"'
-    echo "${3},\"${1}\",\"${2}\""
-}
-
 function prepare_directories {
     mkdir -p "$OUTPUT_DIR/commit_metadata"
     mkdir -p "$OUTPUT_DIR/commit_files"
@@ -72,6 +59,56 @@ function prepare_directories {
     mkdir -p "$OUTPUT_DIR/repository_info"
 }
 
+# Function for downloading the contents of one repository.
+function download_repo_contents {
+    err_echo [[ downloading repo contents ]]
+    local destination=$(mktemp --directory)
+    GIT_TERMINAL_PROMPT=0 git clone "https://github.com/$1/$2.git" "$destination"
+    echo "$destination"
+}
+
+# Functions for retrieving specific bits of information form one repository.
+function retrieve_commit_metadata {
+    err_echo [[ retrieving commit metadata ]]
+    echo '"hash","author name","author email","author timestamp","committer name","committer email","committer timestamp","tag"'
+    git log --pretty=format:'"%H","%an","%ae","%at","%cn","%ce","%ct","%s","%D"' --all 
+}
+function retrieve_commit_file_modification_info {
+    err_echo [[ retrieving commit file modification info ]]
+    echo '"hash","added lines","deleted lines","file"'
+    git log --pretty=format:-----%H:::  --numstat --all | \
+        awk -f "${GHGRABBER_HOME}/awk/numstat.awk"
+}
+function retrieve_commit_file_modification_hashes {
+    err_echo [[ retrieving commit file modification hashes ]]
+    echo '"hash","source file hash","current file hash","status code","file"'
+    git log --format="%n%n%h" --raw --abbrev=40 --all | \
+        awk -f "${GHGRABBER_HOME}/awk/raw.awk"
+}
+function retrieve_commit_comments {
+    err_echo [[ retrieving commit messages ]]
+    echo '"hash","topic","message"'
+    git log --pretty=format:"-----%H:::%s:::%B"  --all | \
+        awk -f "${GHGRABBER_HOME}/awk/comment.awk"
+}
+function retrieve_commit_parents {
+    err_echo [[ retrieving commit parents ]]
+    echo '"child","parent"'
+    git log --pretty=format:"%H %P" | \
+        awk '{for(i=2; i<=NF; i++) print "\""$1"\",\""$i"\""}'
+}
+function retrieve_commit_repositories {
+    err_echo [[ retrieving commit repositories ]]
+    echo '"hash","id"'
+    git log --pretty=format:"\"%H\",$1"
+}
+function retrieve_repository_info {
+    err_echo [[ retrieving repository info ]]
+    echo '"id","user","project"'
+    echo "${3},\"${1}\",\"${2}\""
+}
+
+# Scrape one repository using the functions above.
 function process_repository {
     local user="$1"
     local repo="$2"
@@ -90,7 +127,7 @@ function process_repository {
     retrieve_commit_repositories $i          > "$OUTPUT_DIR/commit_repositories/${filename}"
     retrieve_repository_info $user $repo $i  > "$OUTPUT_DIR/repository_info/${filename}"
 
-    cd "$home"
+    cd "$GHGRABBER_HOME"
 
     if expr ${repository_path} : '/tmp/tmp\...........' >/dev/null
     then
@@ -99,59 +136,58 @@ function process_repository {
     fi
 }
 
-function print_time {
-    local time="$1"
-
-    local hours=$((time/60/60))
-    local minutes=$(((time/60)%60))
-    local seconds=$((time%60))
-
-    local sec_extra_zero=`((seconds > 9)) && echo -n "" || echo -n 0`
-    local min_extra_zero=`((minutes > 9)) && echo -n "" || echo -n 0`
-
-    echo ${hours}:${min_extra_zero}${minutes}:${sec_extra_zero}${seconds}
-}
-
-function use_first_if_available {
-    [ -n "$1" ] && echo "$1" || echo "$2"
-}
-
-REPOS_LIST=`use_first_if_available "$1" "repos.list"`
-OUTPUT_DIR=`use_first_if_available "$2" "data"`
-home="$(pwd)"
-
-if expr "$OUTPUT_DIR" : "^/" >/dev/null 
-then 
-    :
-else 
-    OUTPUT_DIR="$home/$OUTPUT_DIR"
-fi
-
-err_echo "[[ downloading repos from '$REPOS_LIST' to '$OUTPUT_DIR' ]]"
-
-prepare_directories
-echo '"user","repo","time"' > timing.csv
-
-i=0
-for info in `cat "$REPOS_LIST"`
-do
-    i=$(( $i + 1 ))
+# Pre-process arguments and start processing a single repository.
+function download_and_analyze_repository {
+    processed=$(( $processed + 1 ))
+    local info="$1"
 
     if [ -e "STOP" ]
     then
-        err_echo "[[ detected STOP file, stopping ]]"
+        echo [[ detected STOP file, stopping ]]
         break
     fi
 
-    err_echo [[ processing $i $info ]]
+    err_echo [[ processing $processed $info ]]
 
     user=$(echo $info | cut -f1 -d/)
     repo=$(echo $info | cut -f2 -d/)
     
-    start_time=$(date +%s)
-    process_repository $user $repo $i
-    end_time=$(date +%s)
-    echo "\"${user}\"","\"${repo}\"",$(print_time $((end_time - start_time))) \
-        >> "$OUTPUT_DIR/timing.csv"
-done
+    local start_time=$(timing_start)
+    process_repository "$user" "$repo" "$processed"
+    local end_time=$(timing_end)
+
+    sem --id ghgrabber_timing \
+    timing_output "$user" "$repo" "$start_time" "$end_time" "$OUTPUT_DIR/timing.csv"
+}
+
+# Export all the functions that parallel needs.
+export -f timing_init
+export -f timing_start
+export -f timing_end
+export -f timing_print
+export -f timing_output 
+export -f escape_quotes
+export -f err_echo
+export -f use_first_if_available
+export -f prepare_globals
+export -f prepare_directories
+export -f download_repo_contents
+export -f retrieve_commit_metadata
+export -f retrieve_commit_file_modification_info
+export -f retrieve_commit_file_modification_hashes
+export -f retrieve_commit_comments
+export -f retrieve_commit_parents
+export -f retrieve_commit_repositories
+export -f retrieve_repository_info
+export -f process_repository
+export -f download_and_analyze_repository
+
+# Main.
+prepare_globals
+prepare_directories
+timing_init "user" "repo" "$OUTPUT_DIR/timing.csv"
+
+echo [[ downloading repos from "'$REPOS_LIST'" to "'$OUTPUT_DIR'" ]]
+
+<"$REPOS_LIST" parallel -k --lb -j 1 download_and_analyze_repository
 
