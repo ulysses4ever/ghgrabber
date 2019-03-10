7f09233655b92ea507cb88920eb728b1fecb6264
Konrad Siek <konrad.siek@gmail.com>
2019-03-11 00:14:04 +0100

    Updated check info.

--- check.sh
+++ check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+printf ' %2s'    '#'
 printf ' %20s'   'ip'
 printf ' %32s'   'hostname'
 printf ' %10s'   'projects'
@@ -8,6 +9,6 @@ printf ' %32s\n' 'last download'
 for line in `tail -n +2 "$1"`
 do
     IFS=, read user host ip location chunk <<< $(echo "$line")
-    ssh "${user}@${ip}" "printf ' %20s' $ip; printf ' %32s' \`hostname\`\" \"; wc -l < \"$location/timing.csv\" | xargs printf ' %10s'; df -h . | tail -n 1 | tr -s ' ' | cut -f 5 -d ' ' | xargs printf ' %10s'; tail -n 1 \"$location\"/timing.csv | cut -f 4 -d , | TZ=Europe/Prague xargs -I{} date -d @{}  | xargs -I{} printf ' %32s\n' {}"
+    ssh "${user}@${ip}" "printf ' %2s' $chunk; printf ' %20s' $ip; printf ' %32s' \`hostname\`\" \"; wc -l < \"$location/timing.csv\" | xargs printf ' %10s'; df -h . | tail -n 1 | tr -s ' ' | cut -f 5 -d ' ' | xargs printf ' %10s'; tail -n 1 \"$location\"/timing.csv | cut -f 4 -d , | xargs -I {} echo {} / 1000 | bc | TZ=Europe/Prague xargs -I{} date -d @{}  | xargs -I{} printf ' %32s\n' {}"
 done
 
