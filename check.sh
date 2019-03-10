8e3bc1c67be3a6e8bb83ef7afc58ba1fc9a100ca
Konrad Siek <konrad.siek@gmail.com>
2019-03-10 10:27:34 +0100

    Monitoring script

--- check.sh
+++ check.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+printf ' %20s'   'ip'
+printf ' %32s'   'hostname'
+printf ' %10s'   'projects'
+printf ' %10s'   'disk use%'
+printf ' %32s\n' 'last download'
 for line in `tail -n +2 "$1"`
 do
-    IFS=, read user host location <<< $(echo "$line")
-    ssh "${user}@${host}" "echo -n \`hostname\`\" \"; wc -l < \"$location\""
+    IFS=, read user host ip location chunk <<< $(echo "$line")
+    ssh "${user}@${ip}" "printf ' %20s' $ip; printf ' %32s' \`hostname\`\" \"; wc -l < \"$location/timing.csv\" | xargs printf ' %10s'; df -h . | tail -n 1 | tr -s ' ' | cut -f 5 -d ' ' | xargs printf ' %10s'; tail -n 1 \"$location\"/timing.csv | cut -f 4 -d , | TZ=Europe/Prague xargs -I{} date -d @{}  | xargs -I{} printf ' %32s\n' {}"
 done
 
